f245139a4ecfdac5326d624c985a3814d824df17
next215 <next215@gmail.com>
2022-10-08 08:35:37 +0300

    Add work005

--- Work004/Program.cs
+++ Work004/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 1)
+{
+    Console.WriteLine("Число нечётное");
+}
+else
+{
+    Console.WriteLine("Число чётное");
+}
