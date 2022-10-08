a2f8769df51b4ba517dac5b2d12936efef938351
next215 <next215@gmail.com>
2022-10-08 07:20:42 +0300

    Add work003

---
+++ Work003/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число c ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a < b && c < b)
+{
+    Console.WriteLine("Максимальное число = " + b);
+}
+else if (a < c && a < b)
+{
+    Console.WriteLine("Максимальное число = " + c);
+}
+else if (c < a && b < a)
+{
+    Console.WriteLine("Максимальное число = " + a);
+}
+else
+{
+    Console.WriteLine("эти числа равны");
+}
