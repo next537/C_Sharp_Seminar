9bdc5217567f7c96513957c9d65acfdf2d21ca35
next215 <next215@gmail.com>
2022-10-08 11:51:59 +0300

    Add work009

---
+++ Work009/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = new Random().Next(10, 99);
+int b = a / 10;
+int c = a % 10;
+
+Console.WriteLine("Число из отрезка: " + a);
+if (b > c)
+{
+    Console.WriteLine("Наибольшая цифра: " + b);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра: " + c);
+}
+if (b == c)
+{
+    Console.WriteLine("Цифры равны");
+}
