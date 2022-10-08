38258db18c2a444805ee093722a2180dde2f08df
next215 <next215@gmail.com>
2022-10-08 12:47:10 +0300

    Add work012

---
+++ Work012/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число ");
+int a = int.Parse(Console.ReadLine());
+
+if (a / 100 == 0)
+{
+    Console.WriteLine("Введенное число " + a);
+    Console.WriteLine("Третьего числа нет");
+}
+else
+{
+    Console.WriteLine("Введенное число " + a);
+    Console.WriteLine("Третья цифра " + a % 10);
+}
