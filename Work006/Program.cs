ac691fc9cf9a7386177befa18725265d860abe94
next215 <next215@gmail.com>
2022-10-08 09:53:15 +0300

    Add work006

--- Work006/Program.cs
+++ Work006/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+for (int n = a; n <= b; n = n + 1)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write(n + " ");
+    }
+}
