79182eea1133e079054401477a8a9aafa56f388d
next215 <next215@gmail.com>
2022-10-08 09:35:28 +0300

    Add work006

--- Work005/Program.cs
+++ Work005/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("числовой ряд:");
+int b = (a * -1);
+if (a > 0)
+{
+    while (b <= a)
+    {
+        Console.WriteLine(b);
+        b++;
+    }
+}
+else while (a <= b)
+    {
+        Console.WriteLine(a);
+        a++;
+    }
