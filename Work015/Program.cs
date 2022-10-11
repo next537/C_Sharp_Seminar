1cd70699e93e2645f741681f3cc5f748899c09e8
next215 <next215@gmail.com>
2022-10-11 16:47:38 +0300

    Add work015

---
+++ Work015/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите четверть:");
+int x = int.Parse(Console.ReadLine() ?? "0");
+
+if (x == 1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+else if (x == 2)
+{
+    Console.WriteLine("x < 0 и y > 0");
+}
+else if (x == 3)
+{
+    Console.WriteLine("x < 0 и y < 0");
+}
+else if (x == 4)
+{
+    Console.WriteLine("x > 0 и y < 0");
+}
+else
+{
+    Console.WriteLine("Нет такой четверти");
+}
