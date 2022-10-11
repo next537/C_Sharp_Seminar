4409b50627e7bcba9b42013ac056ad4e1bb1c678
next215 <next215@gmail.com>
2022-10-11 15:19:51 +0300

    Add work014

--- Work013/Program.cs
+++ Work013/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите день недели: ");
 int day = int.Parse(Console.ReadLine());
 
-if ((day == 6) && (day == 7))
+if ((day == 6) || (day == 7))
     Console.WriteLine("Этот день недели выходной");
 else
     Console.WriteLine("Этот день недели будни");
