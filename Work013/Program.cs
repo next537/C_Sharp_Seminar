c5b3c568eeaf83e3dc65011b0a145feaf38c6fe4
next215 <next215@gmail.com>
2022-10-12 17:05:50 +0300

    Add work016

--- Work013/Program.cs
+++ Work013/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите день недели: ");
-int day = int.Parse(Console.ReadLine());
+int day = int.Parse(Console.ReadLine() ?? "0");
 
 if ((day == 6) || (day == 7))
     Console.WriteLine("Этот день недели выходной");
---
+++ Work016/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+int b = a / 10000;
+int c = (a / 1000) % 10;
+int d = (a / 10) % 10;
+int e = a % 10;
+
+if (b == e && c == d)
+{
+    Console.WriteLine("Число " + a + " является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число " + a + " не является палиндромом");
+}
