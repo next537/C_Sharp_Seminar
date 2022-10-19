bdb8496c921ac88d16079d3d432bd0f686044440
next215 <next215@gmail.com>
2022-10-19 13:37:59 +0300

    add Work020

---
+++ Work020/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int a = 1;
+while (a <= n)
+{
+    Console.WriteLine("Произведение чисел = " + a * a);
+    a++;
+}
