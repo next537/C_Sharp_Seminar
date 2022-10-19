ab0cc5bf35e91e1a04c8e8b5e0140c3bf950e771
next215 <next215@gmail.com>
2022-10-19 13:20:41 +0300

    add Work018

---
+++ Work018/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double x = double.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+double y = 1;
+while (count <= x)
+{
+    double res = Math.Pow(y, 3);
+    Console.WriteLine($"{res} является кубом {y}");
+    y++;
+    count++;
+}
