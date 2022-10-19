d91e51d811ad7fbd0d1ef6869b57e9e21a4043b9
next215 <next215@gmail.com>
2022-10-19 13:30:59 +0300

    add Work019

---
+++ Work019/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = 0;
+if (a < 0)
+{
+    a = -a;
+}
+while (0 != a)
+{
+    a = a / 10;
+    b++;
+}
+Console.WriteLine("Количество цифр в числе: " + b);
