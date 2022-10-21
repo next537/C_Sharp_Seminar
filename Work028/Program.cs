545d5fd9d2d950553f41a2596abae61b90b7e102
next215 <next215@gmail.com>
2022-10-21 13:05:26 +0300

    Add work028

---
+++ Work028/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 3, 10, 15, 30, 40, 20, 8, 5, 2 };
+int min = array[0];
+int max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+}
+
+Console.WriteLine();
+Console.Write("Разница между max и min = ");
+Console.Write(max - min);
