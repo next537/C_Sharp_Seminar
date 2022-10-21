6646b43b4fc82625d911e23cad60ac069488d023
next215 <next215@gmail.com>
2022-10-21 12:58:43 +0300

    add Work027

---
+++ Work027/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+{
+    Console.Write("Произведение пар чисел = " + array[i] * array[j] + " ");
+    j--;
+}
