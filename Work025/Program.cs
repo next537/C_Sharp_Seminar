b3d61c80aa399935b9dc25640a9550ad5976ed5a
next215 <next215@gmail.com>
2022-10-21 12:02:48 +0300

    Add Work025

---
+++ Work025/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[123];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] >= 10 & array[j] <= 99) count = count + 1;
+}
+Console.WriteLine();
+Console.Write("Количество чисел из отрезка [10,99] = ");
+Console.Write(count);
