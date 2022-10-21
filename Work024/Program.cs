34c4afb87a0a2a56cb6528a331330dd239810c38
next215 <next215@gmail.com>
2022-10-21 11:41:46 +0300

    Add Work024

---
+++ Work024/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[12];
+int a = 0;
+int b = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0) a = a + 1;
+    else b = b + 1;
+}
+
+Console.Write("Нечетных чисел в массиве: " + b);
+Console.WriteLine();
+Console.Write("Четных чисел в массиве: " + a);
+
