aa355dd3c8a122ba12b0d31885096ba6ee71769d
next215 <next215@gmail.com>
2022-10-21 11:30:20 +0300

    Add Work023

---
+++ Work023/Program.cs
@@ -0,0 +1,26 @@
+﻿int sumPlus = 0;
+int sumMinus = 0;
+int[] array = new int[12];
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 9);
+        if (array[i] >= 0) sumPlus = array[i] + sumPlus;
+        else sumMinus = array[i] + sumMinus;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных элементов массива равна {sumPlus}");
+Console.WriteLine($"Сумма отрицательных элементов массива равна {sumMinus}");
