b98df6cf833d70f815a27dd74e3a8a64924b9d09
next215 <next215@gmail.com>
2022-10-21 11:21:07 +0300

    Add work022

---
+++ Work022/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
