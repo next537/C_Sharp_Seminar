b35c8fdf07c71c65120868fd3594669478d99f25
next215 <next215@gmail.com>
2022-10-21 12:13:02 +0300

    add Work026

---
+++ Work026/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[10];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 50);
+    Console.Write(array[i] + " ");
+}
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (j % 2 == 1) count = count + array[j];
+}
+
+Console.WriteLine();
+Console.Write("Сумма числе на нечетных позициях = ");
+Console.Write(count);
