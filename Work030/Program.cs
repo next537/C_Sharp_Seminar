8c2679e25e2562ce3b9bc144032850b7aae44baf
next215 <next215@gmail.com>
2022-10-29 09:47:14 +0300

    Add work031

--- Work030/Program.cs
+++ Work030/Program.cs
@@ -6,16 +6,16 @@ string Scale(double x1, double y1,
                 double x2, double y2,
                 double x3, double y3,
                 double x4, double y4,
-                double n)
+                double k)
 {
-    x1 = x1 * n;
-    y1 = x1 * n;
-    x2 = x2 * n;
-    y2 = y2 * n;
-    x3 = x3 * n;
-    y3 = y3 * n;
-    x4 = x4 * n;
-    y4 = y4 * n;
+    x1 = x1 * k;
+    y1 = x1 * k;
+    x2 = x2 * k;
+    y2 = y2 * k;
+    x3 = x3 * k;
+    y3 = y3 * k;
+    x4 = x4 * k;
+    y4 = y4 * k;
     return $"({x1}, {y1}) ({x2}, {y2}) ({x3}, {y3}) ({x4}, {y4})";
 }
 
---
+++ Work031/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу копирования массива
+
+int[] array = new int[5];
+int arrayCopy;
+
+Console.Write("Массив изначальный - ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 20);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+Console.Write("Результат копирования - ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+}
