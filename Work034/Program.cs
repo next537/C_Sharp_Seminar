1de219915114c929542ee1fb8bee7064f13eb972
next215 <next215@gmail.com>
2022-11-01 17:06:15 +0300

    Add work034

---
+++ Work034/Program.cs
@@ -0,0 +1,51 @@
+﻿//В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадрат
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if ((i % 2 == 0) && (j % 2 == 0))
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int i = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов: ");
+int j = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[i, j];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ReplaceArray(array);
+PrintArray(array);
