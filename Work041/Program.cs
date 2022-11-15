9431d247848264172618e1627e32684c9a0b0d51
next215 <next215@gmail.com>
2022-11-15 16:07:13 +0300

    Add Work041

---
+++ Work041/Program.cs
@@ -0,0 +1,16 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumber(int m, int n)
+{
+    if (n < m) return;
+    PrintNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+
+Console.WriteLine("Введите число M ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+PrintNumber(m, n);
