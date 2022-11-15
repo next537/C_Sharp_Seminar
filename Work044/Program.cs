50d74a67539ca3c435859ea2870f37c428495b26
next215 <next215@gmail.com>
2022-11-15 17:00:58 +0300

    Add work044

---
+++ Work044/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Написать программу показывающие первые N чисел, 
+для которых каждое следующее равно сумме двух предыдущих. 
+Первые два элемента последовательности задаются пользователем*/
+
+int N = 10;
+Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+int Sum(int x, int y, int num)
+{
+    if (num == 1) return x;
+    if (num == 2) return y;
+    return Sum(x, y, num - 1) + Sum(x, y, num - 2);
+}
+for (int i = 1; i < N; i++)
+{
+    Console.Write(Sum(a, b, i) + " ");
+}
+
